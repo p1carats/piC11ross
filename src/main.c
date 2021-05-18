6b74c74b02c68a50316f0fc1841258ebb6b90a45
Laalou <achraf.laalou@student.yncrea.fr>
2021-05-18 11:06:26 +0200

    add nodes

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef struct DListNode {
+	int tab[10];
+	struct DListNode* next;
+	struct DListNode* previous;
+}DListNode;
+
+
+typedef struct DList {
+	int length;
+	struct DListNode* begin;
+	struct DListNode* end; 
+}DList;
+
+
+
+int main() {
+
+
+
+	return 0;
+}
