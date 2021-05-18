5ce530ed8ebb3786920ab3903f277f1902876d4b
Laalou <achraf.laalou@student.yncrea.fr>
2021-05-18 11:51:18 +0200

    add new functions

--- src/main.c
+++ src/main.c
@@ -6,3 +6,17 @@ int main(void){
 	printf("Hello word\n");
 	return 0;
 }
+
+DListNode* newMaillon (void) {
+
+	DListNode* maillon = (DListNode*)malloc(sizeof(*maillon));
+	if (maillon == NULL) {
+		return NULL;
+	}
+
+	maillon->(*tab) = { 0 };
+	maillon->next = NULL;
+	maillon->back = NULL;
+
+	return maillon;
+}
