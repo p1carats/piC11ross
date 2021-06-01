91924eb549ace569d811cfa9cb3b5de03a29a355
Noé Barlet <hello@noebarlet.me>
2021-06-01 16:26:05 +0200

    introducing arguments

--- src/main.c
+++ src/main.c
@@ -1,9 +1,26 @@
 #include "main.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
   srand(time(NULL));
+
+  int size, arg = 0;
+
+  if (argc == 2) {
+    sscanf(argv, "%d", &arg);
+    if (arg != 10 || arg != 15 || arg!= 20) {
+      printf("Incorrect given argument!\n");
+      return 1;
+    } else {
+      size = arg;
+    }
+  } else if (argc > 2) {
+    printf("Too many values given by user.\n");
+    return 1;
+  } else {
+    printf("Single value expected.\n");
+    return 1;
+  }
   
-  int size = 5;
   int *tab = malloc(sizeof(int) * size * size);
   int **listX = malloc(sizeof(int*) * size);
   int **listY = malloc(sizeof(int*) * size);
